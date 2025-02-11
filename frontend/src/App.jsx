8164b51b4238d5359036d92a13b8c7925e61aea4
Rakhmatilla Erkinov <erkinovit@gmail.com>
2025-02-11 14:28:57 +0500

    showed how to use git branching and pull requests

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,21 @@
 import './App.css'
 import Login from "./Pages/Login/index.jsx";
 import {Link, Route, Routes} from "react-router-dom";
+import AccordionExpandIcon from "./pages/join-room/index.jsx";
 
 function App() {
 
-  return (
-    <>
-        <Routes>
-            <Route path="/login" element={<Login />} />
-        </Routes>
-        
-        <Link to="/login">Login</Link>
-    </>
-  )
+    return (
+        <>
+            <Routes>
+                <Route path="/login" element={<Login/>}/>
+
+                <Route path="/join-room" element={<AccordionExpandIcon/>}/>
+            </Routes>
+
+            <Link to="/login">Login</Link>
+        </>
+    )
 }
 
 export default App
