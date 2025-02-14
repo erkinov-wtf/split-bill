927209b1fefea7dda47211c75f1d2a95f9b37a1f
Shohruh Mamatov <mamatovshohruh211@gmail.com>
2025-02-15 01:47:08 +0500

    fixed errors

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,22 @@
 import './App.css'
 import Login from "./Pages/Login/index.jsx";
-import {Link, Route, Routes} from "react-router-dom";
+import { Route, Routes} from "react-router-dom";
+import UsersRoomPage from "./pages/all-users-rooms/usersRoomPage.jsx";
+import Bottombar from "./pages/all-users-rooms/navbar.jsx";
+
 
 function App() {
 
   return (
     <>
+
         <Routes>
             <Route path="/login" element={<Login />} />
+            <Route path="/rooms" element={<UsersRoomPage/>} />
         </Routes>
         
-        <Link to="/login">Login</Link>
+        <Bottombar/>
+
     </>
   )
 }
