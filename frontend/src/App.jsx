48b7f96ed627d1fbe6109836d0290367e7321e2b
Quvonchbek <147691352+Quvonchbek-Qurbonov@users.noreply.github.com>
2025-02-19 17:19:02 +0500

    empty-room-page is done without action

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Login from "./Pages/Login/index.jsx";
 import {Link, Route, Routes} from "react-router-dom";
+import ExpenseApp from "./pages/empty-new-room/emptyNewRoom.jsx";
 
 function App() {
 
@@ -9,8 +10,13 @@ function App() {
         <Routes>
             <Route path="/login" element={<Login />} />
         </Routes>
+
+        <Routes>
+            <Route path="/empty-new-room" element={<ExpenseApp />} />
+        </Routes>
         
         <Link to="/login">Login</Link>
+        <Link to="/empty-new-room">ExpenseApp</Link>
     </>
   )
 }
